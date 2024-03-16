565f1b006f196f599c3cd427cc8ea1e6673b4c4e
Altycon <altycon1@gmail.com>
2024-03-16 03:45:00 -0700

    adding logo transitions

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ import { ANotification } from "./notification.js";
 
 function initializeSite(){
 
+    document.querySelector('.navigation-primary-list').classList.add('reveal');
+
     document.querySelector('.navigation-primary-logo-img').classList.add('reveal');
 }
 initializeSite();
