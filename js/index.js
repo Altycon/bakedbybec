9ab2d55ae393e12b45688ec18c28c2c2769dc660
Altycon <altycon1@gmail.com>
2024-03-16 03:27:39 -0700

    changing pages routes in achor tag

--- js/index.js
+++ js/index.js
@@ -4,40 +4,6 @@ import { ANotification } from "./notification.js";
 
 function initializeSite(){
 
-    [...document.querySelectorAll('.navigation-primary-link')].forEach( link => {
-
-        link.addEventListener('click', (event)=>{
-
-            event.preventDefault();
-
-            const textContent = event.target.textContent;
-
-            switch(textContent){
-
-                case 'product':
-
-                Confirmation.confirm(`This is nothing love. Just stuff I'm adding. Love you. :-)`)
-
-                break;
-
-                case 'contact':
-
-                ANotification.notify('Testing a notification system');
-
-                break;
-
-                case 'meet':
-
-                ANotification.notify('<span style="font-size: 30px;">&#127814;</span>',true);
-
-                break;
-
-                case 'faqs':
-
-                Confirmation.confirm('This is not true.')
-            }
-        })
-    })
-
+    document.querySelector('.navigation-primary-logo-img').classList.add('reveal');
 }
 initializeSite();
