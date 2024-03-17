992099fc0c0de1c51ecd254a8f3d19a5e15f76b1
Altycon <altycon1@gmail.com>
2024-03-17 05:24:14 -0700

    working the pages

--- js/index.js
+++ js/index.js
@@ -7,5 +7,31 @@ function initializeSite(){
     document.querySelector('.navigation-primary-list').classList.add('reveal');
 
     document.querySelector('.navigation-primary-logo-img').classList.add('reveal');
+
+    document.querySelector('.notes-open-btn').addEventListener('click', (event)=>{
+
+        const notes = document.querySelector('.notes');
+
+        notes.querySelector('.notes-close-btn').addEventListener('click', function close(){
+
+            notes.classList.remove('show');
+
+            setTimeout( ()=>{
+                
+                notes.classList.remove('open');
+
+                event.target.removeEventListener('click', close);
+
+            },200)            
+        })
+
+        notes.classList.add('open');
+
+        setTimeout( ()=>{
+            
+            notes.classList.add('show');
+
+        },100)
+    })
 }
 initializeSite();
