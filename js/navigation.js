6ab16d68304138851a40d1751289a16153dde0a4
Altycon <altycon1@gmail.com>
2024-04-20 05:15:48 -0700

    adding a few more images and page navigation

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+
+
+export function pageNavigation(){
+
+    document.querySelector('.page-navigation-primary-open-btn').addEventListener('click', (event)=>{
+        const pageNavigationElement = document.querySelector('.page-navigation-primary');
+        if(pageNavigationElement.classList.contains('open')){
+            event.currentTarget.classList.remove('active');
+            pageNavigationElement.classList.remove('show');
+            setTimeout( ()=> {
+                pageNavigationElement.classList.remove('open');
+            },200);
+        }else{
+            setTimeout( ()=> {
+                pageNavigationElement.classList.add('show');
+            },100);
+            event.currentTarget.classList.add('active');
+            pageNavigationElement.classList.add('open');
+        }
+    });
+
+};
