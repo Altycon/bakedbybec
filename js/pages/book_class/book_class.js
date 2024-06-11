bf174a66d7076e9ab07f5dcda3ab7dac3f57254a
Altycon <altycon1@gmail.com>
2024-06-11 04:33:54 -0700

    book class page mobile style layout update

--- js/pages/book_class/book_class.js
+++ js/pages/book_class/book_class.js
@@ -3,22 +3,12 @@ import { pageNavigation } from "../../navigation.js";
 import { openInHouseBakerySign } from "../../utilities.js";
 
 
-function revealBookClassElements(){
-
-    document.querySelector('.book-class-main').classList.add('reveal');
-};
-
 function initializeBookClassPage(){
 
     pageNavigation();
     document.querySelector('.js-in-house-bakery-btn').addEventListener('click', openInHouseBakerySign);
 
-   // revealBookClassElements();
-
-    let threshold = 0.9;
-    if(innerWidth < 800) threshold = 0.2;
-    
-    createIntersectionObserver('.intersection',threshold);
+    createIntersectionObserver('.intersection',0.9);
     
 };
 
